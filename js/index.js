0eb56a058034298a6045cd9ca8e5ec57b4b44cb1
‘xu’ <laike.xu@shengyc.com>
2021-06-23 16:30:08 +0800

    update

--- js/index.js
+++ js/index.js
@@ -31,114 +31,13 @@ function openPop() {
   if (popupData.protocolName === 'S7_TCP') {
     if (popupData.dataType === '二进制变量') {
       renderS7_TCPHTML([1,2,5], addressData)
-    }
+    } /* else if (popupData.dataType === '有符号8位整型') {
+      addressData.letters = 'MB'
+      renderS7_TCPHTML([1,2], addressData)
+    } */
   }
 
 
-}
-
-
-function renderS7_TCPHTML(items = [], data = {}) {
-   // 1: 数据区域  2. 下拉块（M/DBX/I/Q/MB/DBB/IB/QB/MW/DBW/IW/QW/MD/DBD/ID/QD） 3.  DB号  4. 地址偏移量   5. 位   6. 长度   7.  地址类型 
-  let wrap = document.getElementById('popup-body-wrap')
-  let html = ``
-  if (items.includes(1)) {
-    html +=`
-    <div class="PBW-block" >
-        <div class="PBW-block-item" >
-          <span>数据区域</span>
-          <select onchange="changeData(event, 'dataArea')" >
-            <option value="位" ${data.dataArea === '位' ? 'selected' : ''} >位</option>
-            <option value="DB" ${data.dataArea === 'DB' ? 'selected' : ''} >DB</option>
-            <option value="输入" ${data.dataArea === '输入' ? 'selected' : ''} >输入</option>
-            <option value="输出" ${data.dataArea === '输出' ? 'selected' : ''} >输出</option>
-          </select>
-        </div>
-    </div>
-    `.trim()
-  }
-
-  if (items.includes(2)) {
-    html += `
-      <div class="PBW-block" >
-        <div class="PBW-block-item">
-          <span>${data.letters}</span>
-          <input type="number" min="0" value="${data.lettleValue}" onblur="blurData(event, 'lettleValue')" >
-        </div>
-      </div>
-    `.trim()
-  }
-
-  if (items.includes(3)) {
-    html += `
-      <div class="PBW-block" >
-        <div class="PBW-block-item">
-          <span>DB号</span>
-          <input type="number" min="1" value="${data.DBNum}" onblur="blurData(event, 'DBNum')" >
-        </div>
-      </div>
-    `.trim()
-  }
-
-  if (items.includes(4)) {
-    html += `
-      <div class="PBW-block" >
-        <div class="PBW-block-item">
-          <span>地址偏移量</span>
-          <input type="number" min="1" value="${data.addressOffset}" onblur="blurData(event, 'addressOffset')" >
-        </div>
-      </div>
-    `.trim()
-  }
-
-  if (items.includes(5)) {
-    html += `
-      <div class="PBW-block" >
-        <div class="PBW-block-item">
-          <span>位</span>
-          <select onchange="changeData(event, 'bit')">
-            <option value="0" ${data.bit === '0' ? 'selected' : ''} >0</option>
-            <option value="1" ${data.bit === '1' ? 'selected' : ''} >1</option>
-            <option value="2" ${data.bit === '2' ? 'selected' : ''} >2</option>
-            <option value="3" ${data.bit === '3' ? 'selected' : ''} >3</option>
-            <option value="4" ${data.bit === '4' ? 'selected' : ''} >4</option>
-            <option value="5" ${data.bit === '5' ? 'selected' : ''} >5</option>
-            <option value="6" ${data.bit === '6' ? 'selected' : ''} >6</option>
-            <option value="7" ${data.bit === '7' ? 'selected' : ''} >7</option>
-          </select>
-        </div>
-      </div>
-    `.trim()
-  }
-
-  if (items.includes(6)) {
-    html += `
-      <div class="PBW-block" >
-        <div class="PBW-block-item">
-          <span>长度</span>
-          <input type="number" min="1" max="255" value="${data.len}" onblur="blurData(event, 'len')" >
-        </div>
-      </div>
-    `.trim()
-  }
-
-  if (items.includes(7)) {
-    html += `
-      <div class="PBW-block" >
-        <div class="PBW-block-item">
-          <span>地址类型</span>
-          <select onchange="changeData(event, 'addressType')">
-            <option value="字节" ${data.addressType === '字节' ? 'selected' : ''} >字节</option>
-            <option value="字" ${data.addressType === '字' ? 'selected' : ''} >字</option>
-            <option value="双字" ${data.addressType === '双字' ? 'selected' : ''} >双字</option>
-          </select>
-        </div>
-      </div>
-    `.trim()
-  }
-
-  wrap.innerHTML = html
-  
 }
 
 // 关闭变量弹窗
@@ -153,9 +52,120 @@ function confirmPop () {
   closePop()
 }
 
+
+
+function renderS7_TCPHTML(items = [], data = {}) {
+  // 1: 数据区域  2. 下拉块（M/DBX/I/Q/MB/DBB/IB/QB/MW/DBW/IW/QW/MD/DBD/ID/QD） 3.  DB号  4. 地址偏移量   5. 位   6. 长度   7.  地址类型 
+ let wrap = document.getElementById('popup-body-wrap')
+ let html = ``
+ if (items.includes(1)) {
+   html +=`
+   <div class="PBW-block" >
+       <div class="PBW-block-item" >
+         <span>数据区域</span>
+         <select onchange="changeData(event, 'dataArea')" >
+           <option value="位" ${data.dataArea === '位' ? 'selected' : ''} >位</option>
+           <option value="DB" ${data.dataArea === 'DB' ? 'selected' : ''} >DB</option>
+           <option value="输入" ${data.dataArea === '输入' ? 'selected' : ''} >输入</option>
+           <option value="输出" ${data.dataArea === '输出' ? 'selected' : ''} >输出</option>
+         </select>
+       </div>
+   </div>
+   `.trim()
+ }
+
+ if (items.includes(2)) {
+   html += `
+     <div class="PBW-block" >
+       <div class="PBW-block-item">
+         <span>${data.letters}</span>
+         <input type="number" min="0" value="${data.lettleValue}" onblur="blurData(event, 'lettleValue')" >
+       </div>
+     </div>
+   `.trim()
+ }
+
+ if (items.includes(3)) {
+   html += `
+     <div class="PBW-block" >
+       <div class="PBW-block-item">
+         <span>DB号</span>
+         <input type="number" min="1" value="${data.DBNum}" onblur="blurData(event, 'DBNum')" >
+       </div>
+     </div>
+   `.trim()
+ }
+
+ if (items.includes(4)) {
+   html += `
+     <div class="PBW-block" >
+       <div class="PBW-block-item">
+         <span>地址偏移量</span>
+         <input type="number" min="1" value="${data.addressOffset}" onblur="blurData(event, 'addressOffset')" >
+       </div>
+     </div>
+   `.trim()
+ }
+
+ if (items.includes(5)) {
+   html += `
+     <div class="PBW-block" >
+       <div class="PBW-block-item">
+         <span>位</span>
+         <select onchange="changeData(event, 'bit')">
+           <option value="0" ${data.bit === '0' ? 'selected' : ''} >0</option>
+           <option value="1" ${data.bit === '1' ? 'selected' : ''} >1</option>
+           <option value="2" ${data.bit === '2' ? 'selected' : ''} >2</option>
+           <option value="3" ${data.bit === '3' ? 'selected' : ''} >3</option>
+           <option value="4" ${data.bit === '4' ? 'selected' : ''} >4</option>
+           <option value="5" ${data.bit === '5' ? 'selected' : ''} >5</option>
+           <option value="6" ${data.bit === '6' ? 'selected' : ''} >6</option>
+           <option value="7" ${data.bit === '7' ? 'selected' : ''} >7</option>
+         </select>
+       </div>
+     </div>
+   `.trim()
+ }
+
+ if (items.includes(6)) {
+   html += `
+     <div class="PBW-block" >
+       <div class="PBW-block-item">
+         <span>长度</span>
+         <input type="number" min="1" max="255" value="${data.len}" onblur="blurData(event, 'len')" >
+       </div>
+     </div>
+   `.trim()
+ }
+
+ if (items.includes(7)) {
+   html += `
+     <div class="PBW-block" >
+       <div class="PBW-block-item">
+         <span>地址类型</span>
+         <select onchange="changeData(event, 'addressType')">
+           <option value="字节" ${data.addressType === '字节' ? 'selected' : ''} >字节</option>
+           <option value="字" ${data.addressType === '字' ? 'selected' : ''} >字</option>
+           <option value="双字" ${data.addressType === '双字' ? 'selected' : ''} >双字</option>
+         </select>
+       </div>
+     </div>
+   `.trim()
+ }
+
+ wrap.innerHTML = html
+ 
+}
+
 // 选择下拉内容
 function changeData (e, prop) {
   addressData[prop] = e.target.value
+  if (prop === 'dataArea') {  // 数据区域部分需要重新渲染弹窗html元素
+    if (e.target.value === 'DB') {
+      addressData.letters = 'DBX'
+      renderS7_TCPHTML([1,2,3,5], addressData)
+    }
+  }
 }
 
 function blurData (e, prop) {
