9aeb558a64f32a8424d4efae81c34c36fc9434aa
‘xu’ <laike.xu@shengyc.com>
2021-06-24 10:12:16 +0800

    fix Modbus_TCP 协议

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 // 协议名称： S7_TCP   Modbus_TCP  OPC_DA  OPC_UA  MC3E_Binary_Etherent  MCA1E_Binary_Etherent  Fins_TCP
 var popupData = {
   protocolName: 'Modbus_TCP',
-  dataType: '二进制变量'
+  dataType: '字符串'
 }
 
 // 定义一个提交的数据结构， 用来填写默认值与回显
@@ -32,8 +32,8 @@ function openPop() {
   let pop = document.getElementById('popup')
   pop.style.display = 'block'
 
-  // 1: 数据区域  2. 寄存器字母块（M/DBX/I/Q/MB/DBB/IB/QB/MW/DBW/IW/QW/MD/DBD/ID/QD） 3.  DB号  4. 地址偏移量   5. 位   6. 长度   7.  地址类型 
   if (popupData.protocolName === 'S7_TCP') {    //  渲染 S7_TCP弹窗
+    // 1: 数据区域  2. 寄存器字母块（M/DBX/I/Q/MB/DBB/IB/QB/MW/DBW/IW/QW/MD/DBD/ID/QD） 3.  DB号  4. 地址偏移量   5. 位   6. 长度   7.  地址类型 
     if (popupData.dataType === '二进制变量') {
       formData.showList = formData.showList.length === 0 ?  [1,2,5] : formData.showList
     } else if (popupData.dataType === '有符号8位整型' || popupData.dataType === '无符号8位整型') {
@@ -54,12 +54,19 @@ function openPop() {
     }
     renderS7_TCPHTML(formData.showList, formData, popupData.dataType)
   } else if (popupData.protocolName === 'Modbus_TCP') { //  渲染 Modbus_TCP弹窗
+    // 1. 数据区域  2. 偏移地址   3.位  4. 长度
     let types = ['有符号8位整型','有符号16位整型','有符号32位整型','有符号64位整型','无符号8位整型','无符号16位整型','无符号32位整型','无符号64位整型','F32位浮点数IEEE754','F64位浮点数IEEE754']
     if (popupData.dataType === '二进制变量') {
       formData.showList = formData.showList.length === 0 ?  [1,2] : formData.showList
     }  else if (types.includes(popupData.dataType)) {
+      let areas = ['输入寄存器', '保持寄存器']
+      formData.dataArea = areas.includes(formData.dataArea) ? formData.dataArea : '输入寄存器'
       formData.showList = formData.showList.length === 0 ?  [1,2] : formData.showList
-    }
+    } else if (popupData.dataType === '字符串') {
+      let areas = ['输入寄存器', '保持寄存器']
+      formData.dataArea = areas.includes(formData.dataArea) ? formData.dataArea : '输入寄存器'
+      formData.showList = formData.showList.length === 0 ?  [1,2,4] : formData.showList
+    } 
     renderModbus_TCPHTML(formData.showList, formData, popupData.dataType)
   }
 
@@ -187,22 +194,40 @@ function renderS7_TCPHTML(items = [], data = {}, type) {
 // Modbus_TCP协议 弹窗渲染函数
 function renderModbus_TCPHTML (items = [], data = {}, type) {
   // 1. 数据区域  2. 偏移地址   3.位  4. 长度
+  let types = ['有符号8位整型','有符号16位整型','有符号32位整型','有符号64位整型','无符号8位整型','无符号16位整型','无符号32位整型','无符号64位整型','F32位浮点数IEEE754','F64位浮点数IEEE754', '字符串']
+  /* 
+    types 里面的内容数据区域部分只显示 输入寄存器 和 保持寄存器
+  */
   let wrap = document.getElementById('popup-body-wrap')
   let html = ``
   if (items.includes(1)) {
-    html +=`
-    <div class="PBW-block" >
-        <div class="PBW-block-item" >
-          <span>数据区域</span>
-          <select onchange="changeModule_TCPData(event, 'dataArea', '${type}')" >
-            <option value="线圈状态" ${data.dataArea === '线圈状态' ? 'selected' : ''} >线圈状态</option>
-            <option value="离散输入状态" ${data.dataArea === '离散输入状态' ? 'selected' : ''} >离散输入状态</option>
-            <option value="输入寄存器" ${data.dataArea === '输入寄存器' ? 'selected' : ''} >输入寄存器</option>
-            <option value="保持寄存器" ${data.dataArea === '保持寄存器' ? 'selected' : ''} >保持寄存器</option>
-          </select>
+    if (!types.includes(type)) {
+      html +=`
+      <div class="PBW-block" >
+          <div class="PBW-block-item" >
+            <span>数据区域</span>
+            <select onchange="changeModule_TCPData(event, 'dataArea', '${type}')" >
+              <option value="线圈状态" ${data.dataArea === '线圈状态' ? 'selected' : ''} >线圈状态</option>
+              <option value="离散输入状态" ${data.dataArea === '离散输入状态' ? 'selected' : ''} >离散输入状态</option>
+              <option value="输入寄存器" ${data.dataArea === '输入寄存器' ? 'selected' : ''} >输入寄存器</option>
+              <option value="保持寄存器" ${data.dataArea === '保持寄存器' ? 'selected' : ''} >保持寄存器</option>
+            </select>
+          </div>
+      </div>
+      `.trim()
+    } else {
+      html +=`
+        <div class="PBW-block" >
+            <div class="PBW-block-item" >
+              <span>数据区域</span>
+              <select onchange="changeModule_TCPData(event, 'dataArea', '${type}')" >
+                <option value="输入寄存器" ${data.dataArea === '输入寄存器' ? 'selected' : ''} >输入寄存器</option>
+                <option value="保持寄存器" ${data.dataArea === '保持寄存器' ? 'selected' : ''} >保持寄存器</option>
+              </select>
+            </div>
         </div>
-    </div>
-    `.trim()
+        `.trim()
+    }
   }
   if (items.includes(2)) {
     html += `
@@ -247,12 +272,8 @@ function renderModbus_TCPHTML (items = [], data = {}, type) {
     html += `
       <div class="PBW-block" >
         <div class="PBW-block-item">
-          <span>地址类型</span>
-          <select onchange="changeModule_TCPData(event, 'addressType', '${type}')">
-            <option value="字节" ${data.addressType === '字节' ? 'selected' : ''} >字节</option>
-            <option value="字" ${data.addressType === '字' ? 'selected' : ''} >字</option>
-            <option value="双字" ${data.addressType === '双字' ? 'selected' : ''} >双字</option>
-          </select>
+          <span>长度</span>
+          <input type="number" min="1" max="255" value="${data.len}" onblur="blurData(event, 'len')" >
         </div>
       </div>
     `.trim()
@@ -370,6 +391,7 @@ function changeData (e, prop, type) {
 
 // 选择下拉内容 -- Modbus_TCP协议
 function changeModule_TCPData (e, prop, type) {
+  let types = ['有符号8位整型','有符号16位整型','有符号32位整型','有符号64位整型','无符号8位整型','无符号16位整型','无符号32位整型','无符号64位整型','F32位浮点数IEEE754','F64位浮点数IEEE754']
   formData[prop] = e.target.value
   if (type === '二进制变量') {
     if (prop === 'dataArea') {  // 数据区域部分需要重新渲染弹窗html元素
@@ -381,6 +403,14 @@ function changeModule_TCPData (e, prop, type) {
         formData.showList = [1,2,3]
       } else if (e.target.value === '保持寄存器') {
         formData.showList = [1,2,3]
+      }
+    }
+  } else if (types.includes(type)) {
+    if (prop === 'dataArea') {  // 数据区域部分需要重新渲染弹窗html元素
+       if (e.target.value === '输入寄存器') {
+        formData.showList = [1,2]
+      } else if (e.target.value === '保持寄存器') {
+        formData.showList = [1,2]
       }
     }
   }
