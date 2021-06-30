d885c7354ff4e26efeeae7b4d19a90d54f1a6eb3
‘xu’ <laike.xu@shengyc.com>
2021-06-30 16:17:48 +0800

    update opc table

--- js/index.js
+++ js/index.js
@@ -2432,7 +2432,7 @@ wrap.innerHTML = html
 // OPC DA协议、PC UA协议 弹窗渲染函数
 function renderOPCHTML(items = [], data = {}, type) {
   renderOPCGroup(['组1','组2','组3','组4','组5','组6'], '组1')
-  renderOPCTable([{name: 'Tag_1', dataType: '二进制变量'},{name: 'Tag_2', dataType: '有符号8位整型'},{name: 'Tag_3', dataType: '文本变量8位字符集'},{name: 'Tag_4', dataType: '字符串'}])
+  renderOPCTable([{name: 'Tag_1', dataType: '二进制变量'},{name: 'Tag_2', dataType: '有符号8位整型'},{name: 'Tag_3', dataType: '文本变量8位字符集'},{name: 'Tag_4', dataType: '字符串'}], '二进制变量')
 }
 
 // 渲染OPC协议左侧组列表
@@ -2454,17 +2454,27 @@ function renderOPCGroup(gruopList = [], choice = '') {
   group.innerHTML = html
 }
 // 渲染OPC协议右侧表格
-function renderOPCTable(tableData = []) {
+function renderOPCTable(tableData = [], choice = '') {
   let table = document.getElementById('opc-tbody')
   let html = ``
   tableData.forEach((item,index) => {
+    if (item.dataType === choice) {
+      html += `
+        <tr class="tbody-row active" onclick="choiceRow(event)"  >
+          <td class="table-num">${index + 1}</td>
+          <td>${item.name}</td>
+          <td>${item.dataType}</td>
+        </tr>`
+      .trim()
+    } else {
     html += `
-    <tr>
-      <td class="table-num">${index + 1}</td>
-      <td>${item.name}</td>
-      <td>${item.dataType}</td>
-    </tr>
-    `.trim()
+      <tr class="tbody-row" onclick="choiceRow(event)">
+        <td class="table-num">${index + 1}</td>
+        <td>${item.name}</td>
+        <td>${item.dataType}</td>
+      </tr>`
+    .trim()
+    }
   })
 
   table.innerHTML = html  
@@ -2489,6 +2499,23 @@ function choiceGroup (event) {
     })
   }
 
+}
+
+// 选择表格行
+function choiceRow (event) {
+  let currentRow = event.currentTarget
+  let items = [...document.getElementsByClassName('tbody-row')]
+  if (currentRow.className.includes('active')) {
+    return 
+  } else {
+    items.forEach(item => {
+      if (item === currentRow) {
+        currentRow.classList.add('active')
+      } else {
+        item.classList.remove('active')
+      }
+    })
+  }
 }
 
 // 选择下拉内容 -- S7_TCP协议
